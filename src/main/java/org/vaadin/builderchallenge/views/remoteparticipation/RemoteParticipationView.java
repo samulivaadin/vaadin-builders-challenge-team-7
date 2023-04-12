455333bc9ecfef249fc8c8a3be4a3491b54f565b
Samuli Penttila <samuli@vaadin.com>
2023-04-12 08:05:29 +0300

    Place a map component right smack into the middle of the default page

--- src/main/java/org/vaadin/builderchallenge/views/remoteparticipation/RemoteParticipationView.java
+++ src/main/java/org/vaadin/builderchallenge/views/remoteparticipation/RemoteParticipationView.java
@@ -1,14 +1,11 @@
 package org.vaadin.builderchallenge.views.remoteparticipation;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.map.Map;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 import org.vaadin.builderchallenge.views.MainLayout;
 
 @PageTitle("Remote Participation")
@@ -20,14 +17,8 @@ public class RemoteParticipationView extends VerticalLayout {
     public RemoteParticipationView() {
         setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        Map map = new Map();
+        add(map);
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
