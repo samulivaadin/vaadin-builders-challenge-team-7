162f5864608afef3902de0fe59499d1ffdf0fcd2
Samuli Penttila <samuli@vaadin.com>
2023-04-12 13:51:24 +0300

    Current users geolocation stream

--- src/main/java/org/vaadin/builderchallenge/views/remoteparticipation/RemoteParticipationView.java
+++ src/main/java/org/vaadin/builderchallenge/views/remoteparticipation/RemoteParticipationView.java
@@ -1,5 +1,7 @@
 package org.vaadin.builderchallenge.views.remoteparticipation;
 
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.HtmlComponent;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -15,11 +17,11 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import org.vaadin.builderchallenge.security.AuthenticatedUser;
 import org.vaadin.builderchallenge.views.MainLayout;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 @PageTitle("Remote Participation")
@@ -28,19 +30,26 @@ import java.util.logging.Logger;
 @AnonymousAllowed
 public class RemoteParticipationView extends VerticalLayout {
 
-    private final AuthenticatedUser authenticatedUser;
+    public static final String TESTING_STREAM1 = "https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4";
+    public static final String TESTING_STREAM2 = "https://archive.org/download/Popeye_forPresident/Popeye_forPresident_512kb.mp4";
+
     private static final Logger log = Logger.getLogger(RemoteParticipationView.class.getName());
 
-    public RemoteParticipationView(
-            AuthenticatedUser authenticatedUser
-    ) {
-        this.authenticatedUser = authenticatedUser;
+    private final Map map;
+
+    //Mapping of Coordinates and stream addresses
+    private final HashMap<Coordinate, String> coordinateStreams = new HashMap<>();
+
+    public RemoteParticipationView() {
         setSpacing(false);
 
-        var map = new Map();
+        map = new Map();
         var vaadinHqCoordinates = new Coordinate(22.29985, 60.45234);
         var germanOfficeCoordinates = new Coordinate(13.45489, 52.51390);
         var usOfficeCoordinates = new Coordinate(-121.92163, 37.36821);
+        coordinateStreams.put(vaadinHqCoordinates, TESTING_STREAM1);
+        coordinateStreams.put(germanOfficeCoordinates, TESTING_STREAM1);
+        coordinateStreams.put(usOfficeCoordinates, TESTING_STREAM1);
 
         map.getFeatureLayer().addFeature(getVideoMarkerForCoordinate(vaadinHqCoordinates));
         map.getFeatureLayer().addFeature(getVideoMarkerForCoordinate(germanOfficeCoordinates));
@@ -53,14 +62,14 @@ public class RemoteParticipationView extends VerticalLayout {
                     ev.getMouseDetails().getAbsoluteY())
             );
             var dialog = new Dialog();
-            var video = new AutoplayVideo("https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4");
+            var video = new AutoplayVideo(coordinateStreams.get(coord.getCoordinates()));
             dialog.addThemeName(DialogVariant.LUMO_NO_PADDING.getVariantName());
 
             //Still not possible to do this in a nice way https://github.com/vaadin/flow-components/issues/1173
             dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "align-self", "flex-start");
             dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "position", "absolute");
-            dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "left", ev.getMouseDetails().getAbsoluteX()-200 + "px");
-            dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "top", ev.getMouseDetails().getAbsoluteY()-115 + "px");
+            dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "left", ev.getMouseDetails().getAbsoluteX() - 200 + "px");
+            dialog.getElement().executeJs("this.$.overlay.$.overlay.style[$0]=$1", "top", ev.getMouseDetails().getAbsoluteY() - 115 + "px");
 
             dialog.add(video);
             dialog.open();
@@ -71,6 +80,27 @@ public class RemoteParticipationView extends VerticalLayout {
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        getElement().executeJs("""               
+                if (navigator.geolocation) {
+                    that = this;
+                    navigator.geolocation.getCurrentPosition(pos => {
+                        that.$server.setCurrentClientCoordinates(pos.coords.latitude, pos.coords.longitude);
+                    });
+                }
+                """);
+    }
+
+    @ClientCallable
+    public void setCurrentClientCoordinates(String latitude, String longitude) {
+        log.info("Got coordinates %s %s".formatted(latitude, longitude));
+        Coordinate userCoordinates = new Coordinate(Double.parseDouble(longitude), Double.parseDouble(latitude));
+        coordinateStreams.put(userCoordinates, TESTING_STREAM2);
+        map.getFeatureLayer().addFeature(getVideoMarkerForCoordinate(userCoordinates));
     }
 
     @Tag("video")
@@ -84,13 +114,13 @@ public class RemoteParticipationView extends VerticalLayout {
         }
     }
 
-    private static MarkerFeature getVideoMarkerForCoordinate(Coordinate vaadinHqCoordinates) {
+    private MarkerFeature getVideoMarkerForCoordinate(Coordinate coordinate) {
         var opts = new Icon.Options();
-        opts.setImg(getVideoStream());
-        return new MarkerFeature(vaadinHqCoordinates, new Icon(opts));
+        opts.setImg(getVideoStream(coordinateStreams.get(coordinate)));
+        return new MarkerFeature(coordinate, new Icon(opts));
     }
 
-    private static StreamResource getVideoStream() {
+    private static StreamResource getVideoStream(String streamAddress) {
         return new StreamResource("image.svg",
                 () -> {
                     String svg = """
@@ -110,13 +140,13 @@ public class RemoteParticipationView extends VerticalLayout {
                                 controls="0"
                                 muted="true"
                                 loop="loop"
-                                src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+                                src="%s"
                                 type="video/mp4">
                             </video>
                             </body>
                             </foreignObject>
                             </svg>
-                             """;
+                             """.formatted(streamAddress);
                     return new ByteArrayInputStream(svg.getBytes(StandardCharsets.UTF_8));
                 });
     }
